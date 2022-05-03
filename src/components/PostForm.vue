1ef501617eb461fa4e0f267b83a3317878aad3cb
asurakov <asurakov@gmail.com>
2022-05-03 23:29:17 +0300

    mes

---
+++ src/components/PostForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <form @submit.prevent>
+    <h4>Create post</h4>
+    <my-input v-model="post.title" type="text" placeholder="Title" />
+    <my-input v-model="post.body" type="text" placeholder="Body" />
+    <my-button style="margin-top: 15px" @click="createPost">Create</my-button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+        id: ""
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit("create", this.post);
+      this.post = {
+        title: "",
+        body: "",
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
